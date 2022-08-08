cb3c4afe434ec2a76342d7438602a1af98e8791a
Binsweed <dennan1280@hotmail.com>
2022-08-08 11:15:25 +0200

    ändringar

---
+++ Assets/Scenes/Denni/GameScripts/Interactible.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactible : MonoBehaviour
+{
+    public UnityEvent OnEnter;
+    public UnityEvent OnExit;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OnEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OnExit.Invoke();
+        }
+    }
+
+
+}
