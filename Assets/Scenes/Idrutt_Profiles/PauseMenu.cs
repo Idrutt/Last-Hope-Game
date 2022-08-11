cf0a6967762a18f37c564cfa530b5964701f7829
Idrutt <109948041+Idrutt@users.noreply.github.com>
2022-08-11 09:48:53 +0200

    Meny klar

---
+++ Assets/Scenes/Idrutt_Profiles/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPause = false;
+    public GameObject pauseMenuUi;
+    public GameObject pauseOptionUi;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        pauseOptionUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        pauseOptionUi.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+}
